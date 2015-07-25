e442aa9c4b78aa67b0b9ccacd7e0d8a00ccc34fe
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2015-07-25 16:53:44 +1000

    Tidy up docs/PEP8 etc

--- uqbinfpy/genome.py
+++ uqbinfpy/genome.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 
+
 class GeneExpression:
 
     dataset = ''            # name of data set (if any)
@@ -10,15 +11,25 @@ class GeneExpression:
     default_value_if_null = None # Default value to use if entry is not set (e.g. addSamples may not add values for all genes)
 
     def __init__(self, datasetname='', headerlist=[], genedict={}):
-        """ Create a gene expression data set.
-            The class will store gene names and associated profiles (in which values correspond to "samples").
-            It also stores headers (which names correspond to samples, i.e. experiments).
-            Data should be provided as
-            (0) a name of the set
-            (1) a list of sample names (headerlist; must agree with the number of values in each gene profile)
-            (2) a gene name dictionary where values contain the expression profile (genedict; profile is an iterable with the same number of elements)
-            For example
-            >>> g = GeneExpression("MySet", ['Sample1', 'Sample2'], {'G1': [0.13, 1.23], 'G2': [4.1, -0.9], 'G3': [2.1, -2.1]})
+        """
+        Create a gene expression data set.
+
+        The class will store gene names and associated profiles (in which
+        values correspond to "samples").
+
+        It also stores headers (which names correspond to samples, i.e.
+        experiments).
+
+        :param datasetname: name of data set (if any)
+        :param genes: a list of sample names (headerlist; must agree with the
+                      number of values in each gene profile)
+        :parm genedict: a gene name dictionary where values contain the
+                        expression profile (genedict; profile is an iterable
+                        with the same number of elements)
+
+        For example:
+
+        >>> g = GeneExpression("MySet", ['Sample1', 'Sample2'], {'G1': [0.13, 1.23], 'G2': [4.1, -0.9], 'G3': [2.1, -2.1]})
         """
         self.dataset = datasetname
         self.genes = {}
@@ -37,8 +48,12 @@ class GeneExpression:
             raise RuntimeError("The number of headers (%d) is not equal to the number of samples (%d)" % (len(headerlist), nsamples))
         self.headers = headerlist or ['S%d' % cnt for cnt in range(nsamples)]
 
+
     def _createMatrix(self, genedict):
-        """ Internal method for constructing a numpy matrix from a gene-profile dictionary. """
+        """
+        Internal method for constructing a numpy matrix from a gene-profile
+        dictionary.
+        """
         ngenes = len(self.genes)
         allow_new_genes = False
         if ngenes == 0:  # if instance is empty, include all genes in dict
@@ -70,17 +85,29 @@ class GeneExpression:
                     ndx += 1
         return matrix
 
+
     def getHeaders(self, indices = None):
-        """ Retrieve headers (names of experiments/samples).
-            If indices is None (default), all headers are returned, e.g.
-            >>> g.getHeaders()
-            ['Sample1', 'Sample2']
-            If indices is a single integer, the header for the corresponding entry is returned, e.g.
-            >>> g.getHeaders(1)
-            'Sample2'
-            If indices is an iterable of integers (multiple indices), the list of corresponding headers is returned, e.g.
-            >>> g.getHeaders([1,0])
-            ['Sample2', 'Sample1']
+        """
+        Retrieve headers (names of experiments/samples).
+
+        If indices is None (default), all headers are returned.
+
+        Example:
+
+        >>> g.getHeaders()
+        ['Sample1', 'Sample2']
+
+        If indices is a single integer, the header for the corresponding entry
+        is returned, e.g.
+
+        >>> g.getHeaders(1)
+        'Sample2'
+
+        If indices is an iterable of integers (multiple indices), the list of
+        corresponding headers is returned, e.g.
+
+        >>> g.getHeaders([1,0])
+        ['Sample2', 'Sample1']
         """
         if indices == None:
             return self.headers
@@ -92,18 +119,28 @@ class GeneExpression:
                 ret.append(self.headers[index])
             return ret
 
+
     def getGenes(self, names = None):
-        """ Retrieve applicable gene-profile entries.
-            If names is None (default), all gene names are returned, e.g.
-            >>> g.getGenes()
-            ['G1', 'G2', 'G3']
-            If names is a single string, the profile for the corresponding entry is returned, e.g.
-            >>> g.getGenes('G2')
-            array([ 4.1, -0.9])
-            If names is an iterable of strings (multiple gene names), a dictionary with gene name as key and profile as value is returned.
-            >>> g.getGenes(['G3','G2'])
-            {'G2': array([ 4.1, -0.9]), 'G3': array([ 2.1, -2.1])}
-            """
+        """
+        Retrieve applicable gene-profile entries.
+
+        If names is None (default), all gene names are returned, e.g.
+
+        >>> g.getGenes()
+        ['G1', 'G2', 'G3']
+
+        If names is a single string, the profile for the corresponding entry
+        is returned, e.g.
+
+        >>> g.getGenes('G2')
+        array([ 4.1, -0.9])
+
+        If names is an iterable of strings (multiple gene names), a dictionary
+        with gene name as key and profile as value is returned.
+
+        >>> g.getGenes(['G3','G2'])
+        {'G2': array([ 4.1, -0.9]), 'G3': array([ 2.1, -2.1])}
+        """
         if names == None:
             return self.genes.keys()
         elif isinstance(names, str):
@@ -114,34 +151,52 @@ class GeneExpression:
                 ret[name] = self.matrix[self.genes[name],:]
             return ret
 
+
     def __getitem__(self, ndx):
-        """ Retrieve a specified sample (or a "slice" of samples) for all genes, e.g.
+        """
+        Retrieve a specified sample (or a "slice" of samples) for all genes
+
+        Example:
+
         >>> g[0:2]
-            array([[ 2.1 , -2.1 ],
-                   [ 4.1 , -0.9 ],
-                   [ 0.13,  1.23]])
-            Note that the order of rows/genes is NOT necessarily the same as that used for inserting the data.
+        array([[ 2.1 , -2.1 ],
+               [ 4.1 , -0.9 ],
+               [ 0.13,  1.23]])
+
+        Note that the order of rows/genes is NOT necessarily the same as that
+        used for inserting the data.
         """
         return self.matrix[:,ndx]
 
+
     def getHeaderIndex(self, headers):
-        """ Find the index of the named experiment.
-            Raises a ValueError if not in list. """
+        """
+        Find the index of the named experiment.
+
+        Raises a ValueError if not in list.
+        """
         if isinstance(headers, str):
             return self.headers.index(headers)
         else:
             return [self.headers.index(header) for header in headers]
 
+
     def getSamples(self, samples):
-        """Construct a gene dictionary including only samples in specified indices, e.g.
+        """
+        Construct a gene dictionary including only samples in specified indices
+
+        Examples:
+
         >>> g.getSamples(0)
-            {'G1': 0.13, 'G2': 4.0999999999999996, 'G3': 2.1000000000000001}
+        {'G1': 0.13, 'G2': 4.0999999999999996, 'G3': 2.1000000000000001}
+
         >>> g.getSamples('Sample2')
-            {'G1': 1.23, 'G2': -0.90000000000000002, 'G3': -2.1000000000000001}
+        {'G1': 1.23, 'G2': -0.90000000000000002, 'G3': -2.1000000000000001}
+
         >>> g.getSamples(['Sample2','Sample1'])
-            {'G1': array([ 1.23,  0.13]),
-             'G2': array([-0.9,  4.1]),
-             'G3': array([-2.1,  2.1])}
+        {'G1': array([ 1.23,  0.13]),
+         'G2': array([-0.9,  4.1]),
+         'G3': array([-2.1,  2.1])}
         """
         try:
             index = self.getHeaderIndex(samples)
@@ -152,13 +207,20 @@ class GeneExpression:
             mygenes[name] = self.matrix[ndx, index]
         return mygenes
 
+
     def sort(self, sample, descending=True):
-        """Get a list of gene names, sorted by order of value in specified sample, e.g.
+        """
+        Get a list of gene names, sorted by order of value in specified sample
+
+        Examples:
+
         >>> g.sort(0)
-            ['G2', 'G3', 'G1']
+        ['G2', 'G3', 'G1']
+
         Then retrieve actual genes using e.g.
+
         >>> g.getGenes('G2')
-            array([-0.9,  4.1])
+        array([-0.9,  4.1])
         """
         try:
             index = self.getHeaderIndex(sample)
@@ -175,11 +237,19 @@ class GeneExpression:
                 names.append(name)
         return names
 
+
     def addSamples(self, headerlist, genedict):
-        """Add a sample or multiple samples to the current data set.
-           genedict is a dictionary with the same keys as the current gene set.
-           Only values for genes in the current set will be added (others are ignored).
-           >>> g.addSamples('Sample3', {'G1': 3.4, 'G2': -3.0})
+        """
+        Add a sample or multiple samples to the current data set.
+
+        genedict is a dictionary with the same keys as the current gene set.
+
+        Only values for genes in the current set will be added (others are
+        ignored).
+
+        Examples:
+
+        >>> g.addSamples('Sample3', {'G1': 3.4, 'G2': -3.0})
         """
         newmat = self._createMatrix(genedict)
         nsamples = len(newmat[0])
@@ -194,27 +264,39 @@ class GeneExpression:
             self.matrix = np.hstack((self.matrix, newmat))
         self.headers.extend(headerlist or ['S%d' % cnt+len(self.headers) for cnt in range(nsamples)])
 
+
     def getRatio(self, index1, index2):
-        """ Get the ratio of two samples in the data set (index1 and index2).
-            Creates and returns a gene dictionary with the corresponding ratios. """
+        """
+        Get the ratio of two samples in the data set (index1 and index2).
+
+        Creates and returns a gene dictionary with the corresponding ratios.
+        """
         mygenes = {}
         mdiv = self.matrix[:, index1] / self.matrix[:, index2]
         for (name, ndx) in self.genes.items():
             mygenes[name] = mdiv[ndx]
         return mygenes
 
+
     def getLogRatio(self, index1, index2):
-        """ Get the log2-transformed ratio of two samples (index1 and index2)
-            Creates and returns a gene dictionary with the corresponding log-ratios. """
+        """
+        Get the log2-transformed ratio of two samples (index1 and index2)
+
+        Creates and returns a gene dictionary with the corresponding log-ratios.
+        """
         mygenes = {}
         mlr = np.log2(self.matrix[:, index1] / self.matrix[:, index2])
         for (name, ndx) in self.genes.items():
             mygenes[name] = mlr[ndx]
         return mygenes
 
+
     def getPearson(self, probeID):
-        """ Given a probe identifier, returns a gene/probe dictionary:
-        identifiers to correlation coefficients with the specified probe. """
+        """
+        Given a probe identifier, returns a gene/probe dictionary:
+
+        identifiers to correlation coefficients with the specified probe.
+        """
         index = self.genes[probeID]
         profile = self.matrix[index, :]
         mygenes = {}
@@ -223,8 +305,11 @@ class GeneExpression:
             mygenes[name] = pearson(profile, other)
         return mygenes
 
+
     def writeGEOFile(self, filename):
-        """ Save data as a truncated GEO SOFT file named filename. """
+        """
+        Save data as a truncated GEO SOFT file named filename.
+        """
         line = '^DATASET = ' + self.dataset + '\n'
         line += '!dataset_table_begin\nID_REF\tIDENTIFIER\t'
         for header in self.headers:
@@ -241,11 +326,18 @@ class GeneExpression:
         fh.write(line)
         fh.close()
 
+
     def getZScore(self, index):
-        """ Get the Z-score of each expression value.
-            index can be a list of indices (for which the z-score is computed independently).
-            Important: assumes that values are normally distributed.
-            For example use log-transformed ratios. """
+        """
+        Get the Z-score of each expression value.
+
+        index can be a list of indices (for which the z-score is computed
+        independently).
+
+        Important: assumes that values are normally distributed.
+
+        For example use log-transformed ratios.
+        """
         # Calculate mean and standard deviation of the list of values
         mu = np.mean(self.matrix[:, index], axis=0)
         sd = np.std(self.matrix[:, index], axis=0)
@@ -260,11 +352,14 @@ class GeneExpression:
         # Return the dictionary of Z-scores
         return mygenes
 
+
 # Utility functions
 
 def readGEOFile(filename, id_column=0):
-    """Read a Gene Expression Omnibus file; return a GeneExpression instance.
-    id_column indicates what field of each row that should be taken as
+    """
+    Read a Gene Expression Omnibus file; return a GeneExpression instance.
+
+    id_column indicates what field of each row that should be taken addSamples
     gene identifier.
     """
     fh = open(filename, "rU")
@@ -340,9 +435,12 @@ def readGEOFile(filename, id_column=0):
 # ------------------ Helpful Extra Functions ------------------
 
 def pearson(X, Y):
-    """ Pearson correlation coefficient (r).
+    """
+    Pearson correlation coefficient (r).
+
     Note that we are using the standard deviation of the sample, NOT the sample
-    standard deviation (see http://en.wikipedia.org/wiki/Standard_deviation). """
+    standard deviation (see http://en.wikipedia.org/wiki/Standard_deviation).
+    """
     Xmu = np.mean(X)
     Xvar = np.var(X)
     Ymu = np.mean(Y)
@@ -356,5 +454,4 @@ def pearson(X, Y):
     if n == 0 or Xvar == 0 or Yvar == 0:
         return 0
     return (sum - n * (Xmu * Ymu)) / (n * math.sqrt(Xvar) * math.sqrt(Yvar))
-
 
